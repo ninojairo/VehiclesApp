85ffed548cf2986e0cc40dd92c8939a22a9672a1
ninojairo <nino.jairo@gmail.com>
2018-06-18 23:33:07 -0500

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -71,6 +71,9 @@ var AppComponent = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("./src/app/app.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_vehicles_vehicles_component__ = __webpack_require__("./src/app/components/vehicles/vehicles.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__services_vehicles_service__ = __webpack_require__("./src/app/services/vehicles.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__angular_http__ = __webpack_require__("./node_modules/@angular/http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pipes_keys_pipe__ = __webpack_require__("./src/app/pipes/keys.pipe.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -81,6 +84,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+//Services
+
+
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
@@ -88,12 +95,16 @@ var AppModule = /** @class */ (function () {
         Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
             declarations: [
                 __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
-                __WEBPACK_IMPORTED_MODULE_3__components_vehicles_vehicles_component__["a" /* VehiclesComponent */]
+                __WEBPACK_IMPORTED_MODULE_6__pipes_keys_pipe__["a" /* KeysPipe */],
+                __WEBPACK_IMPORTED_MODULE_3__components_vehicles_vehicles_component__["a" /* VehiclesComponent */],
             ],
             imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_5__angular_http__["b" /* HttpModule */],
             ],
-            providers: [],
+            providers: [
+                __WEBPACK_IMPORTED_MODULE_4__services_vehicles_service__["a" /* VehiclesService */],
+            ],
             bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
         })
     ], AppModule);
@@ -114,7 +125,7 @@ module.exports = ""
 /***/ "./src/app/components/vehicles/vehicles.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<h1>Vehicles</h1>\n<div class=\"container main-container\">\n    <table class=\"table\">\n        <thead>\n            <tr>\n                <th>#</th>\n                <th>First Name</th>\n                <th>Last Name</th>\n                <th>Username</th>\n            </tr>\n        </thead>\n        <tbody>\n            <tr>\n                <th scope=\"row\">1</th>\n                <td>Mark</td>\n                <td>Otto</td>\n                <td>@mdo</td>\n            </tr>\n            <tr>\n                <th scope=\"row\">2</th>\n                <td>Jacob</td>\n                <td>Thornton</td>\n                <td>@fat</td>\n            </tr>\n            <tr>\n                <th scope=\"row\">3</th>\n                <td>Larry</td>\n                <td>the Bird</td>\n                <td>@twitter</td>\n            </tr>\n        </tbody>\n    </table>\n</div>"
+module.exports = "<h1>Vehicles</h1>\n<div class=\"container main-container\">\n    <table class=\"table \">\n        <thead>\n            <tr class=\"table-inverse\">\n                <th>#</th>\n                <th>Marca</th>\n                <th>Modelo</th>\n                <th>Cilindraje</th>\n                <th>Kilometraje</th>\n                <th>Revision TCM</th>\n            </tr>\n        </thead>\n        <tbody>\n            <tr *ngFor=\"let i of vehicles | keys;\">\n                <th scope=\"row\">1</th>\n                <td>{{vehicles[i].marca}}</td>\n                <td>{{vehicles[i].modelo}}</td>\n                <td>@mdo</td>\n                <td>@mdo</td>\n                <td>@mdo</td>\n                <td>@mdo</td>\n            </tr>\n\n        </tbody>\n    </table>\n</div>"
 
 /***/ }),
 
@@ -124,6 +135,7 @@ module.exports = "<h1>Vehicles</h1>\n<div class=\"container main-container\">\n 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return VehiclesComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__services_vehicles_service__ = __webpack_require__("./src/app/services/vehicles.service.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -134,8 +146,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
+
 var VehiclesComponent = /** @class */ (function () {
-    function VehiclesComponent() {
+    function VehiclesComponent(_vehiclesService) {
+        var _this = this;
+        this._vehiclesService = _vehiclesService;
+        this._vehiclesService.getVehicles().subscribe(function (res) {
+            console.log(res);
+            _this.vehicles = res;
+        });
     }
     VehiclesComponent.prototype.ngOnInit = function () {
     };
@@ -145,9 +164,96 @@ var VehiclesComponent = /** @class */ (function () {
             template: __webpack_require__("./src/app/components/vehicles/vehicles.component.html"),
             styles: [__webpack_require__("./src/app/components/vehicles/vehicles.component.css")]
         }),
-        __metadata("design:paramtypes", [])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__services_vehicles_service__["a" /* VehiclesService */]])
     ], VehiclesComponent);
     return VehiclesComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/pipes/keys.pipe.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return KeysPipe; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var KeysPipe = /** @class */ (function () {
+    function KeysPipe() {
+    }
+    KeysPipe.prototype.transform = function (value) {
+        var keys = [];
+        for (var key in value) {
+            keys.push(key);
+        }
+        return keys;
+    };
+    KeysPipe = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["N" /* Pipe */])({
+            name: 'keys'
+        })
+    ], KeysPipe);
+    return KeysPipe;
+}());
+
+// export class KeysPipe implements PipeTransform {
+//   //Value: Objeto con todas las llaves
+//   transform(value: any): any {
+//     let keys =[];
+//     for(let key in value){
+//       keys.push(key);
+//     }
+//     return keys;
+//   }
+// }
+
+
+/***/ }),
+
+/***/ "./src/app/services/vehicles.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return VehiclesService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("./node_modules/@angular/http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_Rx__ = __webpack_require__("./node_modules/rxjs/_esm5/Rx.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var VehiclesService = /** @class */ (function () {
+    function VehiclesService(_http) {
+        this._http = _http;
+        this.URLVehicles = "https://vehiclesapp-da823.firebaseio.com/Vehicles.json";
+    }
+    VehiclesService.prototype.getVehicles = function () {
+        return this._http.get(this.URLVehicles).map(function (res) {
+            console.log(res.json());
+            return res.json();
+        });
+    };
+    VehiclesService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_http__["a" /* Http */]])
+    ], VehiclesService);
+    return VehiclesService;
 }());
 
 
